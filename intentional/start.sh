45057c7758741f33d0d41414f36d86f86e59c221
Matteo Francia <m.francia@unibo.it>
2022-09-12 14:22:46 +0200

    update timeout

--- intentional/start.sh
+++ intentional/start.sh
@@ -6,4 +6,4 @@ if [ -f .env ]; then
 fi
 ./stop.sh
 docker-compose up --build -d
-./wait-for-it.sh ${MYSQL_URL}:${MYSQL_PORT} --strict --timeout=10 -- echo "MySQL is up"
+./wait-for-it.sh ${MYSQL_URL}:${MYSQL_PORT} --strict --timeout=420 -- echo "MySQL is up"
