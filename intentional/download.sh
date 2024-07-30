43957d2b8957e255ecb1ab51920d04fffa3488de
Matteo Francia <m.francia@unibo.it>
2024-07-30 14:19:05 +0200

    Update download.sh

--- intentional/download.sh
+++ intentional/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -exo
 cd resources
-curl -o foodmart-mysql.sql http://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
-curl -o covid_weekly-mysql.sql http://big.csr.unibo.it/projects/nosql-datasets/covid_weekly-mysql.sql
+curl -L -o foodmart-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
+curl -L -o covid_weekly-mysql.sql https://big.csr.unibo.it/projects/nosql-datasets/covid_weekly-mysql.sql
 cd ..
